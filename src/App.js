ed7bcffdb88dd739c7816a9a0547cc9bbe54bd87
liutiki <liutiki@list.ru>
2024-01-31 11:52:12 +0300

    my very first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [advice, setAdvice] = useState('');
+
+
+
+useEffect(() => {
+    getAdvice();
+}, [])
+
+const getAdvice = async () => {
+const response = await fetch(`http://www.boredapi.com/api/activity/`);
+const data = await response.json();
+setAdvice(data.activity)
 }
+
+ const NewAdvice = () => {
+
+ }
+
+return (
+
+<div className="App">
+
+<p className="advice"> {advice} </p>
+
+<button className="btn" onClick={getAdvice}>Get Advice</button>
+
+
+
+</div>
+
+);
+
+}
+
 
 export default App;
